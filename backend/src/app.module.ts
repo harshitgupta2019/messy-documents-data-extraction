5dc6e055c38ce4c0ff30192e16c01b802ced4790
Harshit <hargup3438@gmail.com>
2026-08-21 02:11:22 +0530

    add package

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,1 +1,14 @@
-import {Module} from '@nestjs/common';import {MongooseModule} from '@nestjs/mongoose';import {DocumentsModule} from './documents/documents.module';import {QueryModule} from './query/query.module';@Module({imports:[MongooseModule.forRoot(process.env.MONGO_URL||'mongodb://localhost:27017/docquery'),DocumentsModule,QueryModule]})export class AppModule{}
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { DocumentsModule } from "./documents/documents.module";
+import { QueryModule } from "./query/query.module";
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGO_URL || "mongodb://localhost:27017/docquery",
+    ),
+    DocumentsModule,
+    QueryModule,
+  ],
+})
+export class AppModule {}
